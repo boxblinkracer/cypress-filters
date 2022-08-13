549f8561ae69c37654c5e5a6119d2a53b9432a01
Christian Dangl <boxblinkracer@gmx.net>
2022-08-13 22:39:28 +0200

    improve jest tests

--- tests/Jest/services/FilterParser.spec.js
+++ tests/Jest/services/FilterParser.spec.js
@@ -4,10 +4,29 @@ import FilterParser from '../../../src/services/FilterParser';
 const parser = new FilterParser();
 
 
+test('hasFilters return TRUE on found filter', () => {
+    const filterConfig = parser.getFilters('@smoke');
+    expect(filterConfig.hasFilters()).toBe(true);
+});
+
+test('empty filters string leads to empty list', () => {
+    const filterConfig = parser.getFilters('');
+    expect(filterConfig.hasFilters()).toBe(false);
+});
+
+test('null filter string leads to empty list', () => {
+    const filterConfig = parser.getFilters(null);
+    expect(filterConfig.hasFilters()).toBe(false);
+});
+
+test('simple space string leads to empty list', () => {
+    const filterConfig = parser.getFilters(' ');
+    expect(filterConfig.hasFilters()).toBe(false);
+});
+
+
 test('parsing of 1 filter works', () => {
     const filterConfig = parser.getFilters('@smoke');
-
-    expect(filterConfig.hasFilters()).toBe(true);
 
     expect(filterConfig.getFilters()).toStrictEqual(
         [
@@ -18,8 +37,6 @@ test('parsing of 1 filter works', () => {
 
 test('parsing of 2 OR filters works', () => {
     const filterConfig = parser.getFilters('@smoke @regression');
-
-    expect(filterConfig.hasFilters()).toBe(true);
 
     expect(filterConfig.getFilters()).toStrictEqual(
         [
@@ -32,8 +49,6 @@ test('parsing of 2 OR filters works', () => {
 test('parsing of 3 AND filters works', () => {
     const filterConfig = parser.getFilters('@smoke+@regression+@usability');
 
-    expect(filterConfig.hasFilters()).toBe(true);
-
     expect(filterConfig.getFilters()).toStrictEqual(
         [
             ['@smoke', '@regression', '@usability'],
@@ -45,8 +60,6 @@ test('parsing of 3 AND filters works', () => {
 test('parsing of 2 AND filter groups works', () => {
     const filterConfig = parser.getFilters('@smoke+@regression @usability+@functional');
 
-    expect(filterConfig.hasFilters()).toBe(true);
-
     expect(filterConfig.getFilters()).toStrictEqual(
         [
             ['@smoke', '@regression'],
@@ -55,36 +68,13 @@ test('parsing of 2 AND filter groups works', () => {
     );
 });
 
-
 test('parsing of 2 AND filters and 1 OR filter works', () => {
     const filterConfig = parser.getFilters('@smoke+@regression @usability');
 
-    expect(filterConfig.hasFilters()).toBe(true);
     expect(filterConfig.getFilters()).toStrictEqual(
         [
             ['@smoke', '@regression'],
             ['@usability'],
         ]
     );
-});
-
-test('empty filters string leads to empty list', () => {
-    const filterConfig = parser.getFilters('');
-
-    expect(filterConfig.hasFilters()).toBe(false);
-    expect(filterConfig.getFilters().length).toBe(0);
-});
-
-test('null filter string leads to empty list', () => {
-    const filterConfig = parser.getFilters(null);
-
-    expect(filterConfig.hasFilters()).toBe(false);
-    expect(filterConfig.getFilters().length).toBe(0);
-});
-
-test('simple space string leads to empty list', () => {
-    const filterConfig = parser.getFilters(' ');
-
-    expect(filterConfig.hasFilters()).toBe(false);
-    expect(filterConfig.getFilters().length).toBe(0);
 });
